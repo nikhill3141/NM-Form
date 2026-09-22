f5b48d08d3a4edee54a5487710ecfcd54f625706
Nikhil <nikhilmali3141@gmail.com>
2026-09-22 10:33:11 +0530

    fix: restore env import and update API URL configuration

--- apps/web/trpc/create-client.ts
+++ apps/web/trpc/create-client.ts
@@ -1,5 +1,5 @@
 import { httpLink, httpBatchStreamLink } from "@repo/trpc/client";
-// import { env } from "~/env.js";
+import { env } from "~/env.js";
 
 interface CreateTRPCHttpBatchClientClientOpts {
   enableStreaming?: boolean;
@@ -8,7 +8,7 @@ interface CreateTRPCHttpBatchClientClientOpts {
 export const createTRPCHttpBatchClientClient = (opts?: CreateTRPCHttpBatchClientClientOpts) => {
   const c = opts?.enableStreaming ? httpBatchStreamLink : httpLink;
   return c({
-    url: "http://localhost:8000/trpc",
+    url: env.NEXT_PUBLIC_API_URL || "http://localhost:8000/trpc",
     fetch(url, options) {
       return fetch(url, {
         ...options,
